64c2c2695f7269be6abf6871c04810c5f4753c03
Patricia Aas <psmaas@gmail.com>
2023-05-28 21:47:22 +0200

    Set licence to Dual BSD/GPL

--- exec.bpf.c
+++ exec.bpf.c
@@ -1,7 +1,7 @@
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 
-//char LICENSE[] SEC("license") = "Dual BSD/GPL";
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
 SEC("tp/syscalls/sys_enter_execve")
 int handle_execve(void *ctx)
@@ -9,5 +9,3 @@ int handle_execve(void *ctx)
   bpf_printk("Hello World\n");
   return 0;
 }
-
-char LICENSE[] SEC("license") = "GPL";
